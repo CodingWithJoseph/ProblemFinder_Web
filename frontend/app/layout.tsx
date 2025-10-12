364aa496b81d43a72fc98b033af962d2ab9c06eb
Joseph Howerton <jhowerton450@gmail.com>
2025-10-12 01:54:08 -0700

    updated hero page

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import "../styles/globals.css";
 import { ReactNode } from "react";
-import { Navbar } from "../components/ui/Navbar";
-import { Footer } from "../components/ui/Footer";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
@@ -17,9 +15,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-pf-night text-pf-cloud`}>
         <div className="flex min-h-screen flex-col">
-          <Navbar />
           <main className="flex-1">{children}</main>
-          <Footer />
         </div>
       </body>
     </html>
